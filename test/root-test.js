2aef2b730addbadb5be9764a0f86cedfcd79e014
Todd Wolfson <todd@twolfson.com>
2016-01-08 02:05:45 -0600

    Worked out better syntax for property asserts

--- test/root-test.js
+++ test/root-test.js
@@ -7,6 +7,13 @@ var mprisUtils = require('./utils/mpris');
 describe('A default MPRIS Root interface', function () {
   mprisUtils.init({name: 'root-default'});
 
+  // TODO: Break down into utilities for simpler asserts -- it's going to get a lot more complex
+  //   Initial thoughts
+  //   `it('defaults CanQuit to false', mprisUtils.getProperty('CanQuit', function (CanQuit) {})`
+  //   `mprisUtils.assertProperty('CanQuit', false);` in place of `it`
+  //   `describe('CanQuit', function () { mprisUtils.getProperty('CanQuit'); it('defaults CanQuit')`
+  //   `mprisUtils.getProperties(['CanQuit', 'CanRaise']); it(function () { this.CanQuit; });
+  //   ^^^ winner
   // http://specifications.freedesktop.org/mpris-spec/latest/Media_Player.html#Property:CanQuit
   it('defaults CanQuit to false', function (done) {
     this.mprisSubscriber.GetCanQuit(function handleGetCanQuit (err, CanQuit) {
